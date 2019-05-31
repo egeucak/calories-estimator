45468ed28dacea91076f6d020b352a4743203e97
alibtasdemir <b21627653@cs.hacettepe.edu.tr>
2019-06-01 02:21:36 +0300

    Expanded datasets and improved script

--- scripts/food/getNames.py
+++ scripts/food/getNames.py
@@ -1,12 +1,21 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import csv
+import argparse
 
-url = 'https://www.allrecipes.com/recipes/17562/dinner/?page='
+parser = argparse.ArgumentParser(description='Web scraping for food names.')
+parser.add_argument('url', type=str, help='Base url for food category. Should end with page=#ITERATIVE NUMBER')
+args = parser.parse_args()
+
+if args.url is not None:
+	baseUrl = args.url
+else:
+	baseUrl = 'https://www.allrecipes.com/recipes/1825/world-cuisine/middle-eastern/turkish/?page='
+
 foodNames=set()
 for i in range(1, 50):
-	url += str(i)
-	page_response = requests.get(url, timeout=10)
+	print(f'At page {i}')
+	page_response = requests.get(baseUrl+str(i), timeout=10)
 
 	page_content = bs(page_response.content, "html.parser")
 
@@ -18,7 +27,7 @@ for i in range(1, 50):
 
 print(f'Total {len(foodNames)} andn{foodNames}')
 
-with open('dinnerNames.csv', mode='w') as employee_file:
+with open('TurkishFood.csv', mode='w') as employee_file:
     employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for i, name in enumerate(foodNames):
     	employee_writer.writerow([i, name])
